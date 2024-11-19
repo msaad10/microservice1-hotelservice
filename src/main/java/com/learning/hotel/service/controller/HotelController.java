fb7b94eb008c2a78f7fc1ffaf11857c4461eda8d
msaad10 <saad.muhammad@tenpearls.com>
2024-11-19 15:36:09 +0500

    Update pom.xml to add spring cloud + eureka dependency AND update application.yml to register with eureka server AND minor API change

--- src/main/java/com/learning/hotel/service/controller/HotelController.java
+++ src/main/java/com/learning/hotel/service/controller/HotelController.java
@@ -3,7 +3,6 @@ package com.learning.hotel.service.controller;
 import com.learning.hotel.service.entity.Hotel;
 import com.learning.hotel.service.services.HotelService;
 import jakarta.validation.Valid;
-import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/user")
+@RequestMapping("/v1/hotels")
 public class HotelController {
     private final HotelService hotelService;
 
@@ -25,8 +24,8 @@ public class HotelController {
     }
 
     @PostMapping
-    public ResponseEntity<Hotel> saveUser(@Valid @RequestBody Hotel user) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(user));
+    public ResponseEntity<Hotel> saveHotel(@Valid @RequestBody Hotel hotel) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
     }
 
     @GetMapping("/{hotelId}")
@@ -35,7 +34,7 @@ public class HotelController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Hotel>> getAllUsers() {
+    public ResponseEntity<List<Hotel>> getAllHotels() {
         return ResponseEntity.ok(hotelService.getAll());
     }
 }
